62a9a81672373304c58456d43b2d618fcbb320c5
XAN4X <inkfon23@gmail.com>
2026-02-18 19:33:02 +0700

    Multi Timer Upld

---
+++ multi-script.js
@@ -0,0 +1,43 @@
+let countdown;
+const display = document.getElementById('display');
+const audio = new Audio('hassium.mp3');
+
+function startTimer(seconds) {
+    clearInterval(countdown);
+    audio.pause();
+    audio.currentTime = 0;
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+
+    displayTimeLeft(seconds);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+            audio.play();
+            return;
+        }
+
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const displayStr = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+
+    display.textContent = displayStr;
+    document.title = displayStr + " - Countdown";
+}
+
+function resetTimer() {
+    clearInterval(countdown);
+    audio.pause();
+    audio.currentTime = 0;
+    display.textContent = "0:00";
+    document.title = "Future CountDown";
+}
