609d99d44dcf3f4270e3cab2e06e3af2fca37827
XAN4X <inkfon23@gmail.com>
2026-02-22 21:12:07 +0700

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,15 +1,24 @@
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
+let ampm = document.getElementById("ampm");
 
-setInterval(()=>{
+setInterval(() => {
     let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
 
-    hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
-    min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
-    sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+    let ampmText = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
 
-},1000)
+    hrs.innerHTML = (hours < 10 ? "0" : "") + hours;
+    min.innerHTML = (minutes < 10 ? "0" : "") + minutes;
+    sec.innerHTML = (seconds < 10 ? "0" : "") + seconds;
+    ampm.innerHTML = ampmText;
+
+}, 1000)
 
 
 
